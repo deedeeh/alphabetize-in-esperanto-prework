e6e14f6e939221195b7ca1c33da77fab3cc60ffb
Dina Hafez <dinahafez.8@gmail.com>
2018-08-08 21:36:41 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,7 +4,7 @@ def alphabetize(arr)
   # code here
   esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do |phrase|
-    phrase.split("").collect do |character|
+    phrase.split("").each do |character|
       esperanto_alphabet.index(character)
     end
   end
